8caade07697410636cf2a7f0dd04d7423a608448
Kryxzael <kasper.ramskjell98@gmail.com>
2023-07-08 00:16:11 +0200

    Make player tumble for some time if staling in the air

--- Assets/SeagullsInSpace/Objects/Seagull/FlightController.cs
+++ Assets/SeagullsInSpace/Objects/Seagull/FlightController.cs
@@ -7,12 +7,25 @@ public class FlightController : MonoBehaviour
 {
     private const string FLIGHT_AXIS_NAME = "Horizontal";
 
-    public float RotationSpeed                 = 200f;
-    public float Acceleration                  =   1f;
-    public float RotationCorrectionStrengthMin =  10f;
-    public float RotationCorrectionStrengthMax =  25f;
+    public float RotationSpeed                    =  200f;
+    public float Acceleration                     =    1f;
+    public float RotationCorrectionStrengthMin    =   10f;
+    public float RotationCorrectionStrengthMax    =   25f;
+    public float RotationCorrectionStrengthTumble =  100f;
+    public float TumbleThreshold                  =    0.5f;
+    public float TumbleTime                       =    1f;
 
     private Rigidbody2D _rigidbody;
+
+    private float TumbleTimer = 0;
+
+    public bool IsTumbling
+    {
+        get
+        {
+            return TumbleTimer > 0;
+        }
+    }
 
     private void Awake()
     {
@@ -24,8 +37,11 @@ public class FlightController : MonoBehaviour
         /*
          * Apply user input to rotation
          */
-        float axis = -Input.GetAxisRaw(FLIGHT_AXIS_NAME);
-        transform.Rotate(new Vector3(0, 0, RotationSpeed * axis * Time.deltaTime));
+        if (!IsTumbling)
+        {
+            float axis = Input.GetAxisRaw(FLIGHT_AXIS_NAME);
+            transform.Rotate(new Vector3(0, 0, RotationSpeed * axis * Time.deltaTime));
+        }
 
         /*
          * Apply correction to rotation
@@ -47,12 +63,28 @@ public class FlightController : MonoBehaviour
             sign = -1;
         }
 
-        transform.Rotate(sign * Mathf.Lerp(RotationCorrectionStrengthMin, RotationCorrectionStrengthMax, distanceFromDown / 180f) * Time.deltaTime);
+        if (IsTumbling)
+            transform.Rotate(sign * RotationCorrectionStrengthTumble * Time.deltaTime);
+        else
+            transform.Rotate(sign * Mathf.Lerp(RotationCorrectionStrengthMin, RotationCorrectionStrengthMax, distanceFromDown / 180f) * Time.deltaTime);
 
         /*
          * Apply forward force
          */
         _rigidbody.AddForce(-transform.up * Acceleration);
         _rigidbody.velocity = -transform.up * _rigidbody.velocity.magnitude;
+
+        if (!IsTumbling)
+        {
+            //Player needs to tumble
+            if (_rigidbody.velocity.magnitude < TumbleThreshold)
+                TumbleTimer = TumbleTime;
+        }
+
+        //Player is tumbling. Tick timer
+        else
+        {
+            TumbleTimer -= Time.deltaTime;
+        }
     }
 }
